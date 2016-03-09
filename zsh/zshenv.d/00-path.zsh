840817f43764d9f1f1c4c9017a26525550baa36f
Ivy Evans <ivy@ivyevans.net>
2016-03-09 06:41:12 -0800

    zsh: Add whitelisted ./bin to executables path

--- zsh/zshenv.d/00-path.zsh
+++ zsh/zshenv.d/00-path.zsh
@@ -6,3 +6,7 @@ fi
 
 # Add ~/bin to executables path.
 export PATH="$HOME/bin:$PATH"
+
+# Add *whitelisted* ./bin to executables path.
+# https://twitter.com/tpope/status/165631968996900865
+export PATH=".git/safe/../../bin:$PATH"
