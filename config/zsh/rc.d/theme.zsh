f646fcc5a9768fb1bd85790a041453cb1b6f9919
Ivy Evans <ivy@ivyevans.net>
2018-07-06 19:49:44 +0000

    zsh: Add helpers for switching light/dark themes

--- config/zsh/rc.d/theme.zsh
+++ config/zsh/rc.d/theme.zsh
@@ -1,3 +1,15 @@
+# usage: light! -- Sets terminal color scheme to Solarized light
+light!() {
+  export BG=light
+  eval "$(dircolors "$ZPLUG_HOME/repos/seebi/dircolors-solarized/dircolors.ansi-light")"
+}
+
+# usage: dark! -- Sets terminal color scheme to Solarized dark
+dark!() {
+  export BG=dark
+  eval "$(dircolors "$ZPLUG_HOME/repos/seebi/dircolors-solarized/dircolors.256dark")"
+}
+
 # Solarized dircolors for BSD ls
 # Source: https://github.com/seebi/dircolors-solarized/issues/10#issue-1093648
 if [[ "$OSTYPE" =~ (^darwin|bsd$) ]]; then
@@ -6,5 +18,10 @@ fi
 
 if command -v dircolors &>/dev/null; then
   zplug "seebi/dircolors-solarized", ignore:'*'
-  eval "$(dircolors "$ZPLUG_HOME/repos/seebi/dircolors-solarized/dircolors.256dark")"
+
+  if [ "${BG:-dark}" = "light" ]; then
+    light!
+  else
+    dark!
+  fi
 fi
